cf5cf52e4d696adb95f209a33dc347d3cad0325d
221250108 <221250108@smail.nju.edu.cn>
2025-06-07 17:36:48 +0800

    fix: 修复成就报告生成失败的报错提示

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { router } from './router';
 import axios from 'axios'
 //设置后端地址（本地或服务器），会将请求转发到后端端口
 axios.defaults.baseURL = ('');
-axios.defaults.timeout = 30000;
+axios.defaults.timeout = 120000;
 
 createApp(App).use(ElementPlus).use(router).mount('#app')
